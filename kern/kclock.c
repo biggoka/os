5b82d602fc6f56a76fbfbccd5afbd273970273d4
biggoka <biggoka@gmail.com>
2017-10-10 01:02:32 +0300

    lab4

--- kern/kclock.c
+++ kern/kclock.c
@@ -9,6 +9,19 @@ rtc_init(void)
 	nmi_disable();
 	// LAB 4: your code here
 
+	outb(IO_RTC_CMND, RTC_BREG);
+	uint8_t regB = inb(IO_RTC_DATA);
+	regB = regB | RTC_PIE;
+	outb(IO_RTC_CMND, RTC_BREG);
+	outb(IO_RTC_DATA, regB);
+	outb(IO_RTC_CMND, RTC_AREG);
+	uint8_t regA = inb(IO_RTC_DATA);
+	regA = regA & 0xF0;
+	regA = regA | 0x0F;
+	outb(IO_RTC_CMND, RTC_AREG);
+	outb(IO_RTC_DATA, regA);
+
+
 	nmi_enable();
 }
 
@@ -17,6 +30,9 @@ rtc_check_status(void)
 {
 	uint8_t status = 0;
 	// LAB 4: your code here
+
+	outb(IO_RTC_CMND, RTC_CREG);
+	status = inb(IO_RTC_DATA);
 
 	return status;
 }
